1e84c6f255edec8a0ddaddb57b7ee27866cdc767
PixelSaver <stephen.k@proton.me>
2026-07-06 15:35:27 -0700

    Update custom_tags.gd

--- godot-music-tagger/scenes/custom_tags.gd
+++ godot-music-tagger/scenes/custom_tags.gd
@@ -4,7 +4,10 @@ class_name CustomTagsDisplay
 @export var display_text: RichTextLabel
 @export var button: MenuButton
 @export var line_edit: LineEdit
-signal genre_picked(genre:String)
+signal tags_changed(tags:Array[String])
+var tags : Dictionary[int, Dictionary]= {
+	
+}
 
 func _ready() -> void:
 	button.get_popup().index_pressed.connect(_on_idx_pressed)
@@ -17,13 +20,16 @@ func set_tags(tags:Array[String]) -> void:
 	line_edit.text = out
 	
 
-func set_possible_tags(genres: Array[String]) -> void:
+func set_possible_tags(_tags: Array[String]) -> void:
 	button.get_popup().clear()
-	for genre in genres:
-		button.get_popup().add_item(genre)
+	for tag in _tags:
+		button.get_popup().add_check_item(tag)
+	for idx in button.get_popup().item_count:
+		tags.set(button.get_popup().get_item_id(idx), button.get_popup().get_item_text(idx))
+		
 
 func _on_idx_pressed(idx:int) -> void:
 	var text = button.get_popup().get_item_text(idx)
 	print("Id pressed: %s, %s" % [idx, text])
-	genre_picked.emit(text)
+	#genre_picked.emit(text)
 	set_tags(text)
