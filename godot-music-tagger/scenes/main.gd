5c40ed37977dbf82064c36e3a08874693fbb65f9
PixelSaver <stephen.k@proton.me>
2026-07-08 17:05:11 -0700

    Implemented settings

--- godot-music-tagger/scenes/main.gd
+++ godot-music-tagger/scenes/main.gd
@@ -2,12 +2,20 @@ extends PixelMenuManager
 class_name MusicManager
 
 @onready var music_tagger_node: MusicTaggerNode = $MusicTaggerNode
+@export var settings := TaggerSettings.new()
 
 func _ready() -> void:
-	super()
 	Global.menu_manager = self
 	SignalBus.scan.connect(_on_scan)
+	load_settings(settings)
+	SignalBus.scan.emit(settings.music_directories)
+	super()
 
 func _on_scan(dirs: Array[String]) -> void:
 	if dirs.size() == 0: return
 	music_tagger_node.scan_directory(dirs[0])
+
+func load_settings(s:TaggerSettings):
+	music_tagger_node.cache_directory = s.cache_directory
+	music_tagger_node.music_directories = s.music_directories
+	music_tagger_node.playlist_directory = s.playlist_directory
---
+++ godot-music-tagger/standalones/tagger_settings.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name TaggerSettings
+
+@export var playlist_directory: String = "../test"
+@export var music_directories: Array[String] = ["../test"]
+@export var cache_directory: String = "../test/.cache"
