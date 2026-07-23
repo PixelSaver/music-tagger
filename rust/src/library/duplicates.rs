46386ecadbbff2cae39a4e27e5440928cde1c938
PixelSaver <stephen.k@proton.me>
2026-07-22 23:48:42 -0700

    Duplicates actually works... I didn't purge non-duplicates

--- rust/src/library/duplicates.rs
+++ rust/src/library/duplicates.rs
@@ -2,11 +2,17 @@ use crate::core::models::*;
 use std::collections::HashMap;
 
 pub fn check_duplicates(library: &Library) -> HashMap<String, Vec<TrackLocation>> {
-    let mut duplicates: HashMap<String, Vec<TrackLocation>> = HashMap::new();
+    let mut first_dupe: HashMap<String, Vec<TrackLocation>> = HashMap::new();
     
     for track in &library.tracks {
         let key = track.track.isrc.clone();
-        duplicates.entry(key).or_default().push(track.clone());
+        first_dupe.entry(key).or_default().push(track.clone());
+    }
+    let mut duplicates: HashMap<String, Vec<TrackLocation>> = HashMap::new();
+    for (_, dupe) in first_dupe.iter_mut() {
+        if dupe.len() > 1 {
+            duplicates.insert(dupe[0].track.isrc.clone(), dupe.clone());
+        }
     }
 
     duplicates
