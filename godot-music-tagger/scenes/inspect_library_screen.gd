9479c0e2916ebfc09b3fab4f662a423eb5123248
PixelSaver <stephen.k@proton.me>
2026-07-25 23:59:53 -0700

    My brain hurts from trying to do virtualization... they don't move

--- godot-music-tagger/scenes/inspect_library_screen.gd
+++ godot-music-tagger/scenes/inspect_library_screen.gd
@@ -15,10 +15,14 @@ var _tags: Array[String] = []
 var _idxs: Array[int] = []
 var _selected_idx: int = 0
 
+var _virtual_idx: int = 0
+var _pool: Array[HTrackDisplay] = []
+var _pool_capacity := 0
+
 func _enter_tree() -> void:
 	radial_selector.selected_item_changed.connect(func(idx:int): 
-		_selected_idx = idx
-		call_deferred("_on_selection_changed")
+		_selected_idx = idx + _virtual_idx
+		call_deferred("_on_selection_changed_and_pool")
 	)
 	search_man.search_query_changed.connect(_on_search)
 	search_man.method_requested.connect(_on_method_req)
@@ -43,47 +47,9 @@ func start_anim() -> void:
 func end_anim() -> void: 
 	queue_free()
 
-func _on_method_req(method:SortByContainer.SortMethod) -> void:
-	if method == SortByContainer.SortMethod.RELEVANT:
-		_displayed_tracks = Global.menu_manager.music_tagger_node.get_all_tracks()
-	else:
-		_displayed_tracks = MusicTaggerNode.sort_tracks(_displayed_tracks, method)
-	_set_all_tracks(_displayed_tracks)
-
-	
-
-func _set_all_tracks(all_tracks: Array[GodotTrack]):
-	for child in radial_selector.get_children(): 
-		if child is RichTextLabel: child.queue_free()
-	#await get_tree().process_frame
-	for track in all_tracks:
-		#var label = RichTextLabel.new()
-		#label.custom_minimum_size = Vector2(1000, 100)
-		#label.text = track.track_title
-		#label.mouse_filter = Control.MOUSE_FILTER_PASS
-		#label.visible = false
-		#radial_selector.add_child(label)
-		var display = H_TRACK_DISPLAY.instantiate() as HTrackDisplay
-		display.display_track(track)
-		radial_selector.add_child(display)
-	
-	_genres = Global.menu_manager.music_tagger_node.get_all_genres()
-	_tags = Global.menu_manager.music_tagger_node.get_all_custom_tags()
-	radial_selector.scroll_to_index(0)
-	#song_display.set_genres(_genres)
-	possible_genres.emit(_genres)
-	possible_tags.emit(_tags)
-	#song_display.set_possible_tags(_tags)
-	
-
-func _on_search(query:String, selected_tags:Array[String], selected_genres:Array[String], dupes:bool) -> void:
-	var searched_tracks = Global.menu_manager.music_tagger_node.search_tracks(query, selected_tags, selected_genres, dupes)
-	_displayed_tracks = searched_tracks
-	_idxs = Global.menu_manager.music_tagger_node.searched_track_idxs
-	radial_selector.scroll_angle = 0.
-	radial_selector.target_scroll_angle = 0.
-	print("Query sent")
-	_set_all_tracks(_displayed_tracks)
+func _on_selection_changed_and_pool() -> void:
+	_update_pool_for_selected_index()
+	_on_selection_changed()
 
 func _on_selection_changed() -> void:
 	if _displayed_tracks.size() > 0:
@@ -95,3 +61,89 @@ func _on_selection_changed() -> void:
 			Global.menu_manager.music_tagger_node.get_track_at(track_idx)
 		)
 	#var track = Global.menu_manager.music_tagger_node.get_track_at(track_idx)
+	
+
+func _set_all_tracks(all_tracks: Array[GodotTrack]):
+	_displayed_tracks = all_tracks
+	
+	for child in radial_selector.get_children(): 
+		if child is RichTextLabel or child is HTrackDisplay: child.queue_free()
+	_pool.clear()
+	
+	# how many pooled items needed
+	#var vw: int = radial_selector.visibility_window
+	var vw = 1
+	_pool_capacity = min(_displayed_tracks.size(), vw*2 + 1)
+	if _pool_capacity <= 0: 
+		push_warning("Pool capacity is <= 0")
+		return
+	
+	for i in range(_pool_capacity):
+		var display := H_TRACK_DISPLAY.instantiate() as HTrackDisplay
+		display.visible = false
+		_pool.append(display)
+		radial_selector.add_child(display)
+	
+	radial_selector.scroll_to_index(0)
+	_update_pool_for_selected_index()
+	
+	#for track in all_tracks:
+		#var label = RichTextLabel.new()
+		#label.custom_minimum_size = Vector2(1000, 100)
+		#label.text = track.track_title
+		#label.mouse_filter = Control.MOUSE_FILTER_PASS
+		#label.visible = false
+		#radial_selector.add_child(label)
+		
+		#var display = H_TRACK_DISPLAY.instantiate() as HTrackDisplay
+		#display.display_track(track)
+		#radial_selector.add_child(display)
+	
+	_genres = Global.menu_manager.music_tagger_node.get_all_genres()
+	_tags = Global.menu_manager.music_tagger_node.get_all_custom_tags()
+	radial_selector.scroll_to_index(0)
+	#song_display.set_genres(_genres)
+	possible_genres.emit(_genres)
+	possible_tags.emit(_tags)
+	#song_display.set_possible_tags(_tags)
+
+func _update_pool_for_selected_index() -> void:
+	if _displayed_tracks.is_empty() or _pool.is_empty():
+		push_warning("Displayed tracks or pool was empty")
+		return
+	
+	var idx := radial_selector.get_closest_idx() + _virtual_idx
+	if idx < 0: 
+		return
+	
+	#var vw := radial_selector.visibility_window
+	var vw: int = 1
+	var start_idx := idx - vw
+	for i in range(_pool.size()):
+		var track_idx : int = start_idx + i
+		
+		var d := _pool[i]
+		if track_idx >= 0 and track_idx < _displayed_tracks.size():
+			d.display_track(_displayed_tracks[track_idx])
+			#d.visible = true
+		#else:
+			#d.visible = false
+
+#region Searching 
+func _on_method_req(method:SortByContainer.SortMethod) -> void:
+	if method == SortByContainer.SortMethod.RELEVANT:
+		_displayed_tracks = Global.menu_manager.music_tagger_node.get_all_tracks()
+	else:
+		_displayed_tracks = MusicTaggerNode.sort_tracks(_displayed_tracks, method)
+	_set_all_tracks(_displayed_tracks)
+
+func _on_search(query:String, selected_tags:Array[String], selected_genres:Array[String], dupes:bool) -> void:
+	var searched_tracks = Global.menu_manager.music_tagger_node.search_tracks(query, selected_tags, selected_genres, dupes)
+	_displayed_tracks = searched_tracks
+	_idxs = Global.menu_manager.music_tagger_node.searched_track_idxs
+	radial_selector.scroll_angle = 0.
+	radial_selector.target_scroll_angle = 0.
+	print("Query sent")
+	_set_all_tracks(_displayed_tracks)
+
+#endregion
--- godot-music-tagger/scenes/tag.gd
+++ godot-music-tagger/scenes/tag.gd
@@ -4,5 +4,6 @@ class_name TagDisplay
 
 func set_tag(tag_name: String, tag_color: Color):
 	self.text = tag_name
-	var box = panel.get_theme_stylebox("panel") as StyleBoxFlat
+	var box = panel.get_theme_stylebox("panel").duplicate() as StyleBoxFlat
 	box.bg_color = tag_color
+	panel.add_theme_stylebox_override("panel", box)
