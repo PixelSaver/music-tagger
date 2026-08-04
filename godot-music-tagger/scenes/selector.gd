c782d2976645678a90b930215b713dd0c3a4c2ac
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-08-03 23:43:28 -0700

    Exported to linux as a test, and fixed selector not scaling with window size

--- godot-music-tagger/scenes/selector.gd
+++ godot-music-tagger/scenes/selector.gd
@@ -3,5 +3,7 @@ extends Control
 
 func _ready() -> void:
 	get_viewport().size_changed.connect(_update_size)
+	_update_size()
 func _update_size():
-	$ColorRect.position.x = -get_viewport_rect().size.x*0.45 + $ColorRect.get_combined_minimum_size().x*.5
+	$ColorRect.size = Vector2(get_viewport_rect().size.x*1.2 - 350, 120)
+	#self.custom_minimum_size = Vector2(get_viewport_rect().size.x*1.2 - 400, 100)
